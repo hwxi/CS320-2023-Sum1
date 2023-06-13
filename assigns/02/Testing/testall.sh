56f405f13772911383d4c020c282381444c991c7
Hongwei <gmhwxi@gmail.com>
2023-06-12 21:17:50 -0400

    Updating: very very minorly

---
+++ assigns/02/Testing/testall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sml < assign02-01-test.sml
+sml < assign02-02-test.sml
+sml < assign02-03-test.sml
+sml < assign02-04-test.sml
+sml < assign02-05-test.sml
+sml < assign02-06-test.sml
+sml < assign02-07-test.sml
+
+###### end of [testall.sh] ######
