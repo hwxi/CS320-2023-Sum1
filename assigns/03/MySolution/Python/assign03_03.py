eeb1fd339ccbaa2dd6a4f12615dea48ca45d4bf2
Hongwei <gmhwxi@gmail.com>
2023-06-13 00:19:39 -0400

    Updating: very very minorly

---
+++ assigns/03/MySolution/Python/assign03_03.py
@@ -0,0 +1,22 @@
+####################################################
+#!/usr/bin/env python3
+####################################################
+import sys
+sys.path.append('./../../../../mypylib')
+from mypylib_cls import *
+####################################################
+#
+# HX-2023-06-12: 10 points
+# Please *translate* into Python the SML solution
+# to word_neighbors (which is the one for Assign03-02)
+#
+def word_neighbors(word):
+    """
+    Note that this function should returns a fnlist
+    (not a pylist)
+    Your implementation should be combinator-based very
+    much like the posted solution.
+    """
+    raise NotImplementedError
+#
+####################################################
---
+++ assigns/03/MySolution/SML/assign03-05.sml
@@ -0,0 +1,22 @@
+(* ****** ****** *)
+use
+"./../../../../mysmlib/mysmlib-cls.sml";
+(* ****** ****** *)
+
+(*
+HX-2023-06-12: 10 points
+Please enumerate all the pairs of natural
+numbers. Given pairs (i1, j1) and (i2, j2),
+(i1, j1) should be enumerated ahead of (i2, j2)
+if i1+j1 < i2+j2.
+*)
+
+(* ****** ****** *)
+
+(*
+val theNatPairs: (int*int) stream = fn () => ...
+*)
+
+(* ****** ****** *)
+
+(* end of [CS320-2023-Sum1-assign03-05.sml] *)
