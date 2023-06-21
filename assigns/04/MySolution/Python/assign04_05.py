2aa76b94030fc9b183a72c16bfb33bebe782047e
Hongwei <gmhwxi@gmail.com>
2023-06-20 22:03:41 -0400

    Updating: very very minorly

---
+++ assigns/04/MySolution/Python/assign04_05.py
@@ -0,0 +1,12 @@
+########################
+# HX-2023-06-20: 30 points
+########################
+"""
+Given a history of wordle hints, this function returns a
+word as the player's guess. THIS GUESS SHOULD NOT CONTRADICT
+ANY ONE OF THE HINTS GIVEN.
+"""
+########################################################################
+def wordle_guess(hints):
+    raise NotImplementedError
+########################################################################
--- mypylib/mypylib_cls.py
+++ mypylib/mypylib_cls.py
@@ -150,9 +150,11 @@ def fnlist_pylistize(xs):
 def fnlist_rpylistize(xs):
     return foreach_to_rpylistize(fnlist_foreach)(xs)
 ####################################################
-
+def fnlist_make_map(xs, fopr_func):
+    return foreach_to_map_fnlist(fnlist_foreach)(xs, fopr_func)
+####################################################
 def fnlist_make_pylist(xs): return pylist_fnlistize(xs)
-
+####################################################
 def fnlist_filter_pylist(xs, test_func):
     return foreach_to_filter_pylist(fnlist_foreach)(xs, test_func)
 
